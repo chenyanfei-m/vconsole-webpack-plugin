68573051f773b0d3cdf3603d406869c1528c92e9
chenyanfei <chenyanfei.m@gmail.com>
2019-12-23 20:46:21 +0800

    bug fix

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ const { JSDOM } = require('jsdom')
 const pluginName = 'VConsoleWebpackPlugin'
 const safeTypeReg = /\.html$/
 
-const injectVConsole = (htmlText) => {
+const injectVConsole = (htmlText, version) => {
     const { document } = new JSDOM(htmlText).window
     const vScript1 = document.createElement('script')
-    vScript1.src = 'http://wechatfe.github.io/vconsole/lib/vconsole.min.js?v=3.2.0'
+    vScript1.src = `http://wechatfe.github.io/vconsole/lib/vconsole.min.js?v=${version}`
     const vScript2 = document.createElement('script')
     vScript2.innerHTML = 'window.vConsole = new window.VConsole();'
     document.querySelector('body').appendChild(vScript1)
@@ -21,13 +21,13 @@ class VConsoleWebpackPlugin {
 
     apply(compiler) {
         compiler.hooks.emit.tapAsync(pluginName, (compilation, callback) => {
-            const { enabled, exclude } = this.props
+            const { enabled, exclude, version = '3.2.0' } = this.props
             if (!enabled) return
             const { assets } = compilation
             Object.entries(assets).forEach(([filename, file]) => {
-                if (exclude.test(filename)) return
+                if (exclude && exclude.test(filename)) return
                 if (!safeTypeReg.test(filename)) return
-                const currentHTML = injectVConsole(file.source())
+                const currentHTML = injectVConsole(file.source(), version)
                 compilation.assets[filename] = {
                     source() {
                         return currentHTML
