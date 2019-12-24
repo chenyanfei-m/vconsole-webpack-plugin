6a91b40c4ce3900f82f7d92118c32cd43a190d5e
chenyanfei <chenyanfei.m@gmail.com>
2019-12-24 13:59:32 +0800

    refactor

--- index.js
+++ index.js
@@ -22,22 +22,22 @@ class VConsoleWebpackPlugin {
     apply(compiler) {
         compiler.hooks.emit.tapAsync(pluginName, (compilation, callback) => {
             const { enabled, exclude, version = '3.2.0' } = this.props
-            if (!enabled) return
-            const { assets } = compilation
-            Object.entries(assets).forEach(([filename, file]) => {
-                if (exclude && exclude.test(filename)) return
-                if (!safeTypeReg.test(filename)) return
-                const currentHTML = injectVConsole(file.source(), version)
-                compilation.assets[filename] = {
-                    source() {
-                        return currentHTML
-                    },
-                    size() {
-                        return currentHTML.length
+            if (enabled) {
+                const { assets } = compilation
+                Object.entries(assets).forEach(([filename, file]) => {
+                    if (exclude && exclude.test(filename)) return
+                    if (!safeTypeReg.test(filename)) return
+                    const currentHTML = injectVConsole(file.source(), version)
+                    compilation.assets[filename] = {
+                        source() {
+                            return currentHTML
+                        },
+                        size() {
+                            return currentHTML.length
+                        }
                     }
-                }
-            })
-
+                })
+            }
             callback && callback()
         });
     }
